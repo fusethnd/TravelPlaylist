d2131095d7ffa299ea5a0f328444734d55a6188d
fusethnd <fusethnd@gmail.com>
2024-04-05 12:40:52 +0700

    Initial Commit

---
+++ TravelPlaylist/TravelPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlaylistApp.swift
+//  TravelPlaylist
+//
+//  Created by Thanadon Boontawee on 5/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
